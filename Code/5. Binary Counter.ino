e042f0cca426597ead6ba0be1d1733dd2bba3a9d
PotatoLC <134663368+PotatoLC@users.noreply.github.com>
2024-11-14 19:53:40 -0600

    Create 5. Binary Counter.ino

---
+++ Code/5. Binary Counter.ino
@@ -0,0 +1,145 @@
+const int LED_1 = 13;
+const int LED_2 = 12;
+const int LED_4 = 11;
+const int LED_8 = 10;
+const int LED_RESET = 9;
+const int sleep = 1500;
+const int reset = 5000;
+const int blink = 250;
+
+void setup() {
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  pinMode(LED_4, OUTPUT);
+  pinMode(LED_8, OUTPUT);
+  pinMode(LED_RESET, OUTPUT);
+
+}
+
+void loop() {
+
+  // 0
+  digitalWrite(LED_8, LOW);   // 0
+  digitalWrite(LED_4, LOW);   // 0 
+  digitalWrite(LED_2, LOW);   // 0 
+  digitalWrite(LED_1, LOW);   // 0
+  delay(sleep);
+
+  // 1
+  digitalWrite(LED_8, LOW);   // 0
+  digitalWrite(LED_4, LOW);   // 0 
+  digitalWrite(LED_2, LOW);   // 0 
+  digitalWrite(LED_1, HIGH);  // 1
+  delay(sleep);
+
+  // 2
+  digitalWrite(LED_8, LOW);   // 0 
+  digitalWrite(LED_4, LOW);   // 0
+  digitalWrite(LED_2, HIGH);  // 1
+  digitalWrite(LED_1, LOW);   // 0 
+  delay(sleep);
+
+  // 3
+  digitalWrite(LED_8, LOW);   // 0 
+  digitalWrite(LED_4, LOW);   // 0
+  digitalWrite(LED_2, HIGH);  // 1
+  digitalWrite(LED_1, HIGH);  // 1 
+  delay(sleep);
+
+  // 4
+  digitalWrite(LED_8, LOW);   // 0 
+  digitalWrite(LED_4, HIGH);  // 1
+  digitalWrite(LED_2, LOW);   // 0
+  digitalWrite(LED_1, LOW);   // 0 
+  delay(sleep);
+
+  // 5
+  digitalWrite(LED_8, LOW);   // 0 
+  digitalWrite(LED_4, HIGH);  // 1
+  digitalWrite(LED_2, LOW);   // 0
+  digitalWrite(LED_1, HIGH);  // 1 
+  delay(sleep);
+
+  // 6
+  digitalWrite(LED_8, LOW);   // 0 
+  digitalWrite(LED_4, HIGH);  // 1
+  digitalWrite(LED_2, HIGH);  // 1
+  digitalWrite(LED_1, LOW);   // 0 
+  delay(sleep);
+
+  // 7
+  digitalWrite(LED_8, LOW);   // 0 
+  digitalWrite(LED_4, HIGH);  // 1
+  digitalWrite(LED_2, HIGH);  // 1
+  digitalWrite(LED_1, HIGH);  // 1
+  delay(sleep);
+
+  // 8
+  digitalWrite(LED_8, HIGH);  // 1
+  digitalWrite(LED_4, LOW);   // 0
+  digitalWrite(LED_2, LOW);   // 0
+  digitalWrite(LED_1, LOW);   // 0 
+  delay(sleep);
+
+  // 9
+  digitalWrite(LED_8, HIGH);  // 1 
+  digitalWrite(LED_4, LOW);   // 0
+  digitalWrite(LED_2, LOW);   // 0
+  digitalWrite(LED_1, HIGH);  // 1 
+  delay(sleep);
+
+  // 10
+  digitalWrite(LED_8, HIGH);  // 1
+  digitalWrite(LED_4, LOW);   // 0
+  digitalWrite(LED_2, HIGH);  // 1
+  digitalWrite(LED_1, LOW);   // 0 
+  delay(sleep);
+
+  // 11
+  digitalWrite(LED_8, HIGH);  // 1 
+  digitalWrite(LED_4, LOW);   // 0
+  digitalWrite(LED_2, HIGH);  // 1
+  digitalWrite(LED_1, HIGH);  // 1 
+  delay(sleep);
+
+  // 12
+  digitalWrite(LED_8, HIGH);  // 1 
+  digitalWrite(LED_4, HIGH);  // 1
+  digitalWrite(LED_2, LOW);   // 0
+  digitalWrite(LED_1, LOW);   // 0 
+  delay(sleep);
+
+  // 13
+  digitalWrite(LED_8, HIGH);  // 1 
+  digitalWrite(LED_4, HIGH);  // 1
+  digitalWrite(LED_2, LOW);   // 0
+  digitalWrite(LED_1, HIGH);  // 1 
+  delay(sleep);
+
+  // 14
+  digitalWrite(LED_8, HIGH);  // 1
+  digitalWrite(LED_4, HIGH);  // 1
+  digitalWrite(LED_2, HIGH);  // 1
+  digitalWrite(LED_1, LOW);   // 0 
+  delay(sleep);
+
+  // 15
+  digitalWrite(LED_8, HIGH);  // 1 
+  digitalWrite(LED_4, HIGH);  // 1
+  digitalWrite(LED_2, HIGH);  // 1
+  digitalWrite(LED_1, HIGH);  // 1 
+  delay(sleep);
+
+  digitalWrite(LED_8, LOW);   // 0
+  digitalWrite(LED_4, LOW);   // 0 
+  digitalWrite(LED_2, LOW);   // 0 
+  digitalWrite(LED_1, LOW);   // 0
+  
+  for (int i = 0; i < 6; i++) {
+    digitalWrite(LED_RESET, HIGH); 
+    delay(blink);
+    digitalWrite(LED_RESET, LOW);
+    delay(blink);
+  } 
+  
+}
