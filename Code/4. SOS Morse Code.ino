5c5d2f162945d3468bbfcee42d8e68178e167669
PotatoLC <134663368+PotatoLC@users.noreply.github.com>
2024-11-21 21:48:08 -0600

    Update 4. SOS Morse Code.ino

--- Code/4. SOS Morse Code.ino
+++ Code/4. SOS Morse Code.ino
@@ -1,3 +1,19 @@
+/*
+This Arduino program uses an LED connected to digital pin 8 to signal "SOS" in Morse code.  
+Morse code represents SOS as three dots (...), three dashes (---), and three dots (...).  
+
+The program defines helper functions `dot()` and `dash()` to create the respective patterns.  
+Between letters, a short pause (`SPACE`) is added, and after the entire message,  
+a longer pause (`RESET`) occurs before repeating the sequence.  
+
+Required Materials:
+  - Arduino Uno board
+  - USB power cable
+  - 330-ohm resistor
+  - LED 
+  - Breadboard and connecting wires
+*/
+
 const int GREEN_LED = 8;
 const int SPACE = 900;
 const int RESET = 3200;
@@ -12,7 +28,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  // Mensaje Deseado: SOS --> ... --- ...
+  //  Desired message: SOS --> ... --- ...
 
   dot(3);  // ...
   delay(SPACE);
